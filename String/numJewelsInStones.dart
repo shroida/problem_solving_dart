606c44b489d3e76f7e3153d53e0cc788ba502d33
shroida <shroida012@gmail.com>
2024-12-05 18:23:50 +0200

    add comments

--- String/numJewelsInStones.dart
+++ String/numJewelsInStones.dart
@@ -1,3 +1,11 @@
+//Example 1:
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+
+// Example 2:
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+
 int numJewelsInStones(String jewels, String stones) {
   int result = 0;
   for (int i = 0; i < stones.length; i++) {
