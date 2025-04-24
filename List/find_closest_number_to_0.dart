55920345da37829cadd2d809976d2a29636b94fc
shroida <shroida012@gmail.com>
2025-04-24 13:46:43 +0200

    solve findClosestNumber

--- List/find_closest_number_to_0.dart
+++ List/find_closest_number_to_0.dart
@@ -1,7 +1,16 @@
 int findClosestNumber(List<int> nums) {
-  return 0;
+  if (nums.isEmpty) return 0;
+  int closest = nums[0].abs();
+
+  for (var n in nums) {
+    if (n.abs() < closest) {
+      closest = n.abs();
+    }
+  }
+  return closest;
 }
 
 void main() {
-  
+  int closest = findClosestNumber([-4, 1, 0, 4, 8]);
+  print(closest);
 }
