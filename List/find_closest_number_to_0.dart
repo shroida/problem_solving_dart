e6afad87aafa8df7ce07d0e2f359e9f8d4b570cb
shroida <shroida012@gmail.com>
2025-04-24 14:13:38 +0200

    solve it another way

--- List/find_closest_number_to_0.dart
+++ List/find_closest_number_to_0.dart
@@ -1,13 +1,21 @@
 int findClosestNumber(List<int> nums) {
   if (nums.isEmpty) return 0;
-  int closest = nums[0].abs();
 
-  for (var n in nums) {
-    if (n.abs() < closest) {
-      closest = n.abs();
+  int closestNum = nums[0]; //-4
+  int minDistance = nums[0].abs(); //4
+
+  for (int num in nums) {
+    int currentDistance = num.abs();
+    if (currentDistance < minDistance) {
+      minDistance = currentDistance;
+      closestNum = num;
+    } else if (currentDistance == minDistance) {
+      if (num > closestNum) {
+        closestNum = num;
+      }
     }
   }
-  return closest;
+  return closestNum;
 }
 
 void main() {
